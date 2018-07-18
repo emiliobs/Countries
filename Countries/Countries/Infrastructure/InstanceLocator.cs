aa7eef49725e453b1568a0e16ac671359a5b845f
emilio barrera <barrera_emilio@hotmail.com>
2018-07-18 23:14:23 +0100

    patron locator

---
+++ Countries/Countries/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,29 @@
+﻿namespace Countries.Infrastructure
+{
+    using Countries.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InstanceLocator
+    {
+
+        #region Properties
+
+        public MainViewModel Main { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+        #endregion
+
+
+
+    }
+}
