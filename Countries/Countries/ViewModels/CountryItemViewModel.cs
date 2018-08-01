fc49bf0963bea7e209663db11076d9c58c5a41b0
emilio barrera <barrera_emilio@hotmail.com>
2018-08-01 11:57:22 +0100

    All ContryTabbedPage

--- Countries/Countries/ViewModels/CountryItemViewModel.cs
+++ Countries/Countries/ViewModels/CountryItemViewModel.cs
@@ -19,7 +19,9 @@
         {
             //con el this paso toda la clase que viene en constructor des de el origen:
             MainViewModel.GetInstance().Country = new CountryViewModel(this);
-            await Application.Current.MainPage.Navigation.PushAsync(new CountryView());
+            //await Application.Current.MainPage.Navigation.PushAsync(new CountryView());
+            await Application.Current.MainPage.Navigation.PushAsync(new CountryTabbedPage());
+
         }
 
         #endregion
---
+++ Countries/Countries/ViewModels/CountryTabbedPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Countries.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class CountryTabbedPageViewModel
+    {
+    }
+}
