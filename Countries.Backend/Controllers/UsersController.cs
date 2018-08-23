0d9c4455f19840db72673d8c4196bfe351a7b070
emilio barrera <barrera_emilio@hotmail.com>
2018-08-23 11:43:15 +0100

    New user with roles

--- Countries.Backend/Controllers/UsersController.cs
+++ Countries.Backend/Controllers/UsersController.cs
@@ -6,6 +6,8 @@
     using System.Web.Mvc;
     using Countries.Backend.Models;
     using Countries.Domain.Models;
+    using System;
+    using Countries.Backend.Helpers;
 
     [Authorize(Roles = "Admin")]
     public class UsersController : Controller
@@ -44,16 +46,37 @@
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "UserId,FirstName,LastName,Email,Telephone,ImagePath")] User user)
+        public async Task<ActionResult> Create(UserView view)
         {
             if (ModelState.IsValid)
             {
+                var user = this.ToUser(view);
+
+                //aqui creo el nuevo usuario:
                 db.Users.Add(user);
                 await db.SaveChangesAsync();
+
+                //aqui genero el usurio asp con role:
+                UsersHelper.CreateUserASP(view.Email, "User", view.Password);
+
                 return RedirectToAction("Index");
             }
 
-            return View(user);
+            return View(view);
+        }
+
+        //aqui recibo un userview y devuelvo un user:
+        private User ToUser(UserView view)
+        {
+            return new User()
+            {
+              Email = view.Email,
+              FirstName = view.FirstName,
+              ImagePath = view.ImagePath,
+              LastName = view.LastName,
+              Telephone = view.Telephone,
+              UserId = view.UserId,
+            };
         }
 
         // GET: Users/Edit/5
