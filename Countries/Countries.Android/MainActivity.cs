bdeb7ee1334303d9e1931fec47ad7461f14063ab
emilio barrera <barrera_emilio@hotmail.com>
2018-09-05 01:08:05 +0100

    add camare nutgets

--- Countries/Countries.Android/MainActivity.cs
+++ Countries/Countries.Android/MainActivity.cs
@@ -9,6 +9,7 @@ namespace Countries.Droid
     using Android.Widget;
     using Android.OS;
     using FFImageLoading.Forms.Droid;
+    using Plugin.CurrentActivity;
 
     [Activity(Label = "Countries", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
@@ -21,8 +22,14 @@ namespace Countries.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
             CachedImageRenderer.Init(true);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
